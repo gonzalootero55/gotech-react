3f12ab12e34b824b069cde92a347979cd37fb67a
Gonzalo Otero <gonzalo.otero55@gmail.com>
2025-12-13 22:16:15 -0300

    Fix: Corregida ruta de importación del componente Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
 import Gallery from "./components/Gallery";
